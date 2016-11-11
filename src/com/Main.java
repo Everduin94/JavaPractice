47ce2ca617166bccb11bb0038491419b81f9c063
Erik Verduin <everduin94@gmail.com>
2016-11-11 09:48:35 -0600

    Effective Java Builder Example

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,8 +1,15 @@
 package com;
 
+import com.chapter2effectivejava.NutritionFacts;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Builder example
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+        cocaCola.printAllNutritionFacts();
+
     }
 }
